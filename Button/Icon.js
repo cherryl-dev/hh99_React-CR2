bc21287aa9ea16df55aec8fd428e12de621671e0
cherryl-dev <155723499+cherryl-dev@users.noreply.github.com>
2024-03-02 00:44:47 +0900

    Delete / 잘못 생성된 폴더 삭제

--- Button/Icon.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const Icon = styled.img`
-    width: ${(props) => (props.small ? '12px' : '15px')};
-    height: ${(props) => (props.small ? '12px' : '15px')};
-    margin-left: 5px;
-`;
-
-export default Icon;
